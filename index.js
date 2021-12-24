a659c4327eefd62f658d9dc42df2904243a08af6
Mohammad Khalid Kassem Agha <mkhalid.k.agha@gmail.com>
2021-12-24 21:28:20 +0200

    step 4

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 var express = require('express')
 var app = express()
 var port = 2500
-
+//home url or no entry url
 app.get('/', (req, res) => {
     res.send('OK');
     console.log("You went to this url ---> " + req.url);
 })
-
+app.get('/home', (req, res) => {
+    res.send('OK');
+    console.log("You went to this url ---> " + req.url);
+})
+//test url
 app.get('/test', (req, res) => {
     var status = 200;
-    var message="ok";
+    var message = "ok";
     res.send(`{ status : ${status} , message:${message} }`)
 });
-
-
-app.get('/time' ,(req,res)=>{
+//time url
+app.get('/time', (req, res) => {
     var status = 200;
-    var today= new Date();
+    var today = new Date();
     var time = `${today.getHours()}:${today.getSeconds()}`
     res.send(`{ status : ${status} , message:${time} }`)
 });
-
+//ID url
+app.get('/hello/:id', (req, res) => {
+    var status = 200;
+    res.send(`{ status : ${status} , message: Hello , ${req.params.id} }`)
+});
+//search url
+app.get('/search/:date', (req, res) => {
+    if (req.params.date === "item") {
+        var status = 200;
+        res.send(`{ status : ${status} , message: OK , data: ${req.params.date} }`)
+    }
+    else {
+        var error = true
+        var status = 500;
+        res.send(`{ status : ${status} , Error : ${error} , message: you have to provide a search }`)
+    }
+});
 
 app.listen(port, () => {
     console.log(`Khalid's server is listening to  http://localhost:${port}`)
