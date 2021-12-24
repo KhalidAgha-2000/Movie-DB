e0beded62582372773e05877ef251c97c71734eb
Mohammad Khalid Kassem Agha <mkhalid.k.agha@gmail.com>
2021-12-24 20:41:49 +0200

    step 3

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     console.log("You went to this url ---> " + req.url);
 })
 
+app.get('/test', (req, res) => {
+    var status = 200;
+    var message="ok";
+    res.send(`{ status : ${status} , message:${message} }`)
+});
+
+
+app.get('/time' ,(req,res)=>{
+    var status = 200;
+    var today= new Date();
+    var time = `${today.getHours()}:${today.getSeconds()}`
+    res.send(`{ status : ${status} , message:${time} }`)
+});
+
 
 app.listen(port, () => {
     console.log(`Khalid's server is listening to  http://localhost:${port}`)
