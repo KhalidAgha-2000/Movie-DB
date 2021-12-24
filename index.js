406b0e52da27e4ac290fbff6e018d61a7a3aba29
Mohammad Khalid Kassem Agha <mkhalid.k.agha@gmail.com>
2021-12-24 20:41:12 +0200

    step 2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 2500
+var express = require('express')
+var app = express()
+var port = 2500
 
 app.get('/', (req, res) => {
-  res.send('OK')
+    res.send('OK');
+    console.log("You went to this url ---> " + req.url);
 })
 
+
 app.listen(port, () => {
-  console.log(`Khalid's server is listening to  http://localhost:${port}`)
+    console.log(`Khalid's server is listening to  http://localhost:${port}`)
 })
