bab4631e770cd9bef8d4068e623a194bfabf8af4
KhalidAgha-2000 <mkhalid.k.agha@gmail.com>
2021-12-30 14:02:57 +0200

    step 3

--- index.js
+++ index.js
@@ -1,105 +1,31 @@
-var express = require('express')
-const { json, send } = require('express/lib/response')
-var app = express()
-var port = 2500
+var express = require("express");
+var bodyParser = require("body-parser");
+// var moviesRout = require('./MOVIES/movies.js')
+var app = express();
+
+var routerM = express.Router();
 
 var movies = [
-    { id: 1, title: 'Jaws', year: 1975, rating: 8 },
-    { id: 2, title: 'Avatar', year: 2009, rating: 7.8 },
-    { id: 3, title: 'Brazil', year: 1985, rating: 8 },
-    { id: 4, title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
+  { id: 1, title: "Jaws", year: 1975, rating: 8 },
+  { id: 2, title: "Avatar", year: 2009, rating: 7.8 },
+  { id: 3, title: "Brazil", year: 1985, rating: 8 },
+  { id: 4, title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
+var port = 2500;
+app.use(bodyParser.json());
+app.use("/Movies", routerM);
+//Step 3
+routerM.get("/test", (req, res) => {
+  res.send(`{status : ${(res.status = 200)} , message : OK}`);
+});
+routerM.get("/time", (req, res) => {
+  var today = new Date();
+  var time = `${today.getHours()}:${today.getSeconds()}`;
+  res.send(`{status : ${(res.status = 200)} , message : ${time}}`);
+});
+//Step 3
 
-//home url or no entry url
-app.get('/', (req, res) => {
-    res.send('OK');
-    console.log("You went to this url ---> " + req.url);
-})
-app.get('/home', (req, res) => {
-    res.send('OK');
-    console.log("You went to this url ---> " + req.url);
-})
-//test url
-app.get('/test', (req, res) => {
-    var status = 200;
-    var message = "ok";
-    res.send(`{ status : ${status} , message:${message} }`)
+app.get("/", (req, res) => {
+  res.send("Hello First !!!!!!");
 });
-//time url
-app.get('/time', (req, res) => {
-    var status = 200;
-    var today = new Date();
-    var time = `${today.getHours()}:${today.getSeconds()}`
-    res.send(`{ status : ${status} , message:${time} }`)
-});
-//ID url
-app.get('/hello/:id', (req, res) => {
-    var status = 200;
-    res.send(`{ status : ${status} , message: Hello , ${req.params.id} }`)
-});
-//search url
-app.get('/search/:date', (req, res) => {
-    if (req.params.date === "item") {
-        var status = 200;
-        res.send(`{ status : ${status} , message: OK , data: ${req.params.date} }`)
-    }
-    else {
-        var error = true
-        var status = 500;
-        res.send(`{ status : ${status} , Error : ${error} , message: you have to provide a search }`)
-    }
-});
-//url Movies
-app.get('/movies', (req, res) => {
-    // var status=200;
-    res.send(`{status : 200 , message : Movies}`)
-})
-//url add/create Movies
-app.get('/movies/add', (req, res) => {
-    // var status=200;
-    res.send(`{status : 200 , message : Add Movies}`)
-});
-
-//url get/read Movies
-app.get('/movies/get', (req, res) => {
-    res.send({ "Statue": 200, movies })
-});
-//url edit/update Movies
-app.get('/movies/edit', (req, res) => {
-    // var status=200;
-    res.send(`{status : 200 , message : edit Movies}`)
-});
-//url delete Movies
-app.get('/movies/delete', (req, res) => {
-    // var status=200;
-    res.send(`{status : 200 , message : delete Movies}`)
-});
-//url get by rating
-app.get('/movies/get/by-rate', (req, res) => {
-    var sortRate = movies.sort((a, b) => {
-        return b.rating - a.rating
-    })
-    res.send({ "status ": 200, "data:": sortRate })
-});
-//url get by date
-app.get('/movies/get/by-date', (req, res) => {
-    const sortYear = movies.sort((a, b) => (a.year > b.year ? 1 : -1))
-    res.send({ "status": 200, "data: ": sortYear })
-});
-//url get by title
-app.get('/movies/get/by-title', (req, res) => {
-    const sortTitle = movies.sort((a, b) => (a.title > b.title ? 1 : -1))
-    res.send({ "status": 200, "data: ": sortTitle })
-
-});
-
-//movie by id
-app.get('/movies/get/id/:id', (req, res) => {
-    const moviee = movies.find(m => m.id === parseInt(req.params.id));
-    if (!moviee) res.status(404).send(`{status:404, error:true, message:'the movie ${req.params.id} does not exist'}`)
-    res.status(200).send(`{status:200, data: title : ${moviee.title} ,Year : ${moviee.year} , Rate :  ${moviee.rating} }`)
-});
-
-app.listen(port, () => {
-    console.log(`Khalid's server is listening to  http://localhost:${port}`)
-});
+app.listen(port, () => console.log(`Runing to http://localhost:${port}`));
